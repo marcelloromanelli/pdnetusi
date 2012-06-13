7159dce5af5f63b4eaafe478de7c4793fc356c07
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-06-13 15:13:35 +0200

    ps fix

--- public/applications/photostream/tiles/js/8_5_ws.js
+++ public/applications/photostream/tiles/js/8_5_ws.js
@@ -1,3 +1,33 @@
+$(function() { 
+	var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
+	var wsUri = "ws://pdnet.inf.unisi.ch:9000/photostream/socket";
+	websocket = new WS(wsUri); 
+	websocket.onopen = function(evt) { 
+		console.log("CONNECTED"); 
+		var hi = JSON.stringify
+		({
+			"kind":"tileAvailable",
+			"displayID":  getUrlVars()["id"],
+			"width": 8,
+			"height":5
+		});
+		websocket.send(hi);
+		console.log(hi);
+	}; 
+	
+	websocket.onclose = function(evt) { 
+		console.log("DISCONNECTED"); 
+	};
+	
+	websocket.onmessage = function(evt) { 
+		var response = jQuery.parseJSON(evt.data);
+	};
 
+	websocket.onerror = function(evt) { 
+		console.log(evt.data); 
+	}; 
+  	
+
+});
 
 	
