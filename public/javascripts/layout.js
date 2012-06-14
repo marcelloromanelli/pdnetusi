d588ec5914163734c739d4b32c24beed4f9bf629
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-06-14 13:16:17 +0200

    fix css

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -13,7 +13,8 @@ function createApp(appName, id, startX, startY, width, height,htmlSource){
 		top: startY*SIZE+'px',
 		width: width*SIZE+'px',
 		height: height*SIZE+'px',
-		border: "0"
+		border: "0",
+        user-select: 'none'
 	});	
 
 	tile.css('background-color','red');
