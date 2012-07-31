0ec949f3dc223757dbb9826d91b97ec7c6408dd8
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-07-31 16:06:11 +0200

    lol

--- public/applications/weather/js/ws_small.js
+++ public/applications/weather/js/ws_small.js
@@ -17,12 +17,12 @@ $(function() {
 		
 		var response = jQuery.parseJSON(evt.data);
 		$("#first_location").html(response.location.city);
-		$("#first_current_temp").html(response.condition.temperature + "ผ");
+		$("#first_current_temp").html(response.condition.temperature + "ยบ");
 		$("#first_humidity").html(response.atmosphere.humidity);
 		$("#first_wind_speed").html(response.wind.speed);
 		$("#first_wind_direction").html(response.wind.direction);
-		$("#first_maxtemp").html(response.forecast[0].high_temperature + "ผ");
-		$("#first_mintemp").html(response.forecast[0].low_temperature + "ผ");
+		$("#first_maxtemp").html(response.forecast[0].high_temperature + "ยบ");
+		$("#first_mintemp").html(response.forecast[0].low_temperature + "ยบ");
 
 		console.log("SERVER APP ANSWER: ");
 		console.log(response) ;
