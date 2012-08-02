882aa54754aa618199a0627e52dccccedca14c67
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-02 13:29:10 +0200

    queueu

--- public/applications/weather/js/ws_small.js
+++ public/applications/weather/js/ws_small.js
@@ -1,12 +1,19 @@
-// TEST
+status = [true, true, true];
+
 $(function() { 
-	displayID = getUrlVars()["id"]
+	displayID = getUrlVars()["id"];
 	var WS = WebSocket;
 	var wsUri = "ws://pdnet.inf.unisi.ch:9000/weather/socket";
 	websocket = new WS(wsUri); 
 	websocket.onopen = function(evt) { 
 		console.log("CONNECTED"); 
-		sendHiMessage();
+		var hi = JSON.stringify
+		({
+			"kind":"appReady",
+			"displayID":  displayID,
+			"size": "small"
+		});
+		websocket.send(hi);
 	}; 
 
 	websocket.onclose = function(evt) { 
@@ -14,16 +21,10 @@ $(function() {
 	};
 
 	websocket.onmessage = function(evt) {
-		
-		var response = jQuery.parseJSON(evt.data);
-		$("#first_location").html(response.location.city);
-		$("#first_current_temp").html(response.condition.temperature + "º");
-		$("#first_humidity").html(response.atmosphere.humidity);
-		$("#first_wind_speed").html(response.wind.speed);
-		$("#first_wind_direction").html(response.wind.direction);
-		$("#first_maxtemp").html(response.forecast[0].high_temperature + "º");
-		$("#first_mintemp").html(response.forecast[0].low_temperature + "º");
 
+		var response = jQuery.parseJSON(evt.data);
+		
+		console.log(findFree());
 		console.log("SERVER APP ANSWER: ");
 		console.log(response) ;
 	};
@@ -35,65 +36,25 @@ $(function() {
 });
 
 
-/* Send initial message to the websocket of the
- * corresponding app in order to notify its availability.
- */
-function sendHiMessage(){
-	var hi = JSON.stringify
-	({
-		"kind":"appReady",
-		"displayID":  displayID,
-		"size": "small"
-	});
-	websocket.send(hi);
+function findFree(){
+	for (var idx in status) {  
+		if(status[idx]){
+			status[idx] = false;
+			return idx;
+		} else {
+			console.log("ERROR: TO MANY REQUESTS");
+		}
+	}
 }
 
-function loadDefaultParameters(tileID){
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET","http://pdnet.inf.unisi.ch:9000/assets/displays/list.xml" ,false);
-	xmlhttp.send();
-	console.log("DISPLAY ID:" + displayID);
-	var xmlDoc=xmlhttp.responseXML;
-	var displays = xmlDoc.getElementsByTagName("display");
-	layoutID = null;
-	for(var j=0; j<displays.length; j++){
-		var currentDisplay = displays[j];
-		var currentDisplayID = currentDisplay.getElementsByTagName("id")[0].childNodes[0].nodeValue;
-		if (currentDisplayID == displayID){
-			layoutID =  currentDisplay.getElementsByTagName("layoutID")[0].childNodes[0].nodeValue;
-			break;
-		}
-	}
-
-
-
-	xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET","http://pdnet.inf.unisi.ch:9000/assets/displays/layouts/"+layoutID+".xml" ,false);
-	xmlhttp.send();
-	xmlDoc=xmlhttp.responseXML;
-	var tiles = xmlDoc.getElementsByTagName("tile");
-	for(var i=0; i<tiles.length; i++)
-	{
-		var currentTile = tiles[i];
-		var currentTileID = currentTile.getElementsByTagName("id")[0].childNodes[0].nodeValue;
-		if (currentTileID == tileID){
-			var params = currentTile.getElementsByTagName("parameter");
-			for(var j=0; j<params.length;j++){
-				var paramName = params[j].childNodes[0].nodeValue;
-				var paramValue = params[j].getAttribute("value");
-				var defaultRequest = JSON.stringify
-				({
-					"kind":"defaultRequest",
-					"displayID":  displayID,
-					"preference" : paramValue
-				});
-				websocket.send(defaultRequest);
-				console.log("SENDING DEFAULT REQUEST ");
-				console.log(defaultRequest);
-
-			}
-		}
-	}
+function updateFirst(evt){
+	$("#first_location").html(response.location.city);
+	$("#first_current_temp").html(response.condition.temperature + "º");
+	$("#first_humidity").html(response.atmosphere.humidity);
+	$("#first_wind_speed").html(response.wind.speed);
+	$("#first_wind_direction").html(response.wind.direction);
+	$("#first_maxtemp").html(response.forecast[0].high_temperature + "º");
+	$("#first_mintemp").html(response.forecast[0].low_temperature + "º");
 }
 
 function getUrlVars()
@@ -110,5 +71,5 @@ function getUrlVars()
 }
 
 function lowerWithoutSpaces(input){
-    return input.toLowerCase().split(' ').join('');
+	return input.toLowerCase().split(' ').join('');
 }
