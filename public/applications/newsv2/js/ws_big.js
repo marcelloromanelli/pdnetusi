c25b0496ee90fc0361895c7f88208e43713a0e29
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-14 16:26:15 +0200

    test

--- public/applications/newsv2/js/ws_big.js
+++ public/applications/newsv2/js/ws_big.js
@@ -281,8 +281,8 @@ function fadeQR(event){
 $.fn.animateHighlight = function(highlightColor, duration) {
     var highlightBg = highlightColor || "#FFFF9C";
     var animateMs = duration || 1500;
-    var originalBg = this.css("backgroundColor");
-    this.stop().css("background-color", highlightBg).animate({backgroundColor: originalBg}, animateMs);
+    var originalBg = this.css("background-color");
+    this.stop().css("background-color", highlightBg).animate({"background-color": originalBg}, animateMs);
 };
 
 function freeSpace(){
