cc9db4cc97715cf0d1af235876e3b155147ebcfc
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-17 15:13:27 +0200

    fix

--- public/applications/newsv2/js/ws_big.js
+++ public/applications/newsv2/js/ws_big.js
@@ -69,9 +69,11 @@ function moveNews(){
 	//GET ALL THE NEWS
 	$(".news").animate({"top":"+="+total}, { duration: 1000, easing: "linear"});
 	// CHECK SAFTEY MARGINS
-	positionOfFirst = $($(".news")[0]).position.top;
 	//TOP - 4th element
 	var allNews = $(".news"); 
+	
+	positionOfFirst = $(allNews).position().top;
+	positionOfLast = $(allNews[allNews.length-1]).position().top;
 	if(allNews.length > 5){
 		var canaryTop = $(allNews[0]).position().top;
 		if(canaryTop == -1490){
