881244f2933c3036aa067c00b88e58c10de13d76
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-24 15:16:15 +0200

    complete

--- public/applications/weather/js/ws_big.js
+++ public/applications/weather/js/ws_big.js
@@ -66,58 +66,64 @@ function updateFirst(response,cityname){
 		}
 
 	});
-	
+
 	city.animate({"margin-top": original},'fast');
-	city.animate({"margin-top": "400px" }, {duration: 'slow',easing: 'easeInBounce'});
+	city.effect("bounce", { times:5 }, 300);
 }
 
 function updateSecond(response,cityname){
 	var city = $($(".city").get(1));
 	var original = city.css("margin-top");
-	city.animate({"margin-top": "-749px"},'fast');
+	city.animate({"margin-top": "-749px"},'slow',function(){
 
-	$("#second_city").html(cityname);
+		$("#second_city").html(cityname);
 
-	$("#second_img").attr("src","css/icons/"+response.condition.code +".png");
-	$("#second_current_temp").html(response.condition.temperature + "º");
-	$("#second_humidity").html(response.atmosphere.humidity);
-	$("#second_wind_speed").html(response.wind.speed);
-	$("#second_wind_direction").html(response.wind.direction);
-	$("#second_maxtemp").html(response.forecast[0].high_temperature + "º");
-	$("#second_mintemp").html(response.forecast[0].low_temperature + "º");
+		$("#second_img").attr("src","css/icons/"+response.condition.code +".png");
+		$("#second_current_temp").html(response.condition.temperature + "º");
+		$("#second_humidity").html(response.atmosphere.humidity);
+		$("#second_wind_speed").html(response.wind.speed);
+		$("#second_wind_direction").html(response.wind.direction);
+		$("#second_maxtemp").html(response.forecast[0].high_temperature + "º");
+		$("#second_mintemp").html(response.forecast[0].low_temperature + "º");
 
-	for(var i = 2; i < 6; i++){
-		$("#second_d" + i + "_day_name").html(response.forecast[i-1].day);
-		$("#second_d" + i + "_desc").html(response.forecast[i-1].condition);
-		$("#second_d" + i + "_maxtemp").html(response.forecast[i-1].high_temperature + "º");
-		$("#second_d" + i + "_mintemp").html(response.forecast[i-1].low_temperature + "º");
-	}
+		for(var i = 2; i < 6; i++){
+			$("#second_d" + i + "_day_name").html(response.forecast[i-1].day);
+			$("#second_d" + i + "_desc").html(response.forecast[i-1].condition);
+			$("#second_d" + i + "_maxtemp").html(response.forecast[i-1].high_temperature + "º");
+			$("#second_d" + i + "_mintemp").html(response.forecast[i-1].low_temperature + "º");
+		}
+	});
 
-	city.animate({"margin-top": original},'slow');
+	city.animate({"margin-top": original},'fast');
+	city.effect("bounce", { times:5 }, 300);
 
 }
 
 function updateThird(response,cityname){
 	var city = $($(".city").get(2));
 	var original = city.css("margin-top");
-	city.animate({"margin-top": "-749px"},'fast');
+	city.animate({"margin-top": "-749px"},'slow',function(){
 
-	$("#third_city").html(cityname);
+		$("#third_city").html(cityname);
 
-	$("#third_img").attr("src","css/icons/"+response.condition.code +".png")
-	$("#third_current_temp").html(response.condition.temperature + "º");
-	$("#third_humidity").html(response.atmosphere.humidity);
-	$("#third_wind_speed").html(response.wind.speed);
-	$("#third_wind_direction").html(response.wind.direction);
-	$("#third_maxtemp").html(response.forecast[0].high_temperature + "º");
-	$("#third_mintemp").html(response.forecast[0].low_temperature + "º");
+		$("#third_img").attr("src","css/icons/"+response.condition.code +".png")
+		$("#third_current_temp").html(response.condition.temperature + "º");
+		$("#third_humidity").html(response.atmosphere.humidity);
+		$("#third_wind_speed").html(response.wind.speed);
+		$("#third_wind_direction").html(response.wind.direction);
+		$("#third_maxtemp").html(response.forecast[0].high_temperature + "º");
+		$("#third_mintemp").html(response.forecast[0].low_temperature + "º");
 
-	for(var i = 2; i < 6; i++){
-		$("#third_d" + i + "_day_name").html(response.forecast[i-1].day);
-		$("#third_d" + i + "_desc").html(response.forecast[i-1].condition);
-		$("#third_d" + i + "_maxtemp").html(response.forecast[i-1].high_temperature + "º");
-		$("#third_d" + i + "_mintemp").html(response.forecast[i-1].low_temperature + "º");
-	}
+		for(var i = 2; i < 6; i++){
+			$("#third_d" + i + "_day_name").html(response.forecast[i-1].day);
+			$("#third_d" + i + "_desc").html(response.forecast[i-1].condition);
+			$("#third_d" + i + "_maxtemp").html(response.forecast[i-1].high_temperature + "º");
+			$("#third_d" + i + "_mintemp").html(response.forecast[i-1].low_temperature + "º");
+		}
 
-	city.animate({"margin-top": original},'slow');
+	});
+
+	city.animate({"margin-top": original},'fast');
+	city.effect("bounce", { times:5 }, 300);
+
 }
