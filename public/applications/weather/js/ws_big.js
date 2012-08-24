a2a6a7608a5db1b9008c4320741ae8e8885885f6
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-24 15:10:41 +0200

    fix

--- public/applications/weather/js/ws_big.js
+++ public/applications/weather/js/ws_big.js
@@ -68,7 +68,7 @@ function updateFirst(response,cityname){
 	});
 	
 	city.animate({"margin-top": original},'fast');
-	city..animate({ margin-top: 200 }, {duration: 'slow',easing: 'easeOutBounce'});
+	city.animate({ margin-top: 200 }, {duration: 'slow',easing: 'easeOutBounce'});
 }
 
 function updateSecond(response,cityname){
