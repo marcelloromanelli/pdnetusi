93a6b0564498dec1f21e3fd223d7c661a343e591
Marcello Romanelli <marcello.romanelli@usi.ch>
2012-08-30 15:29:06 +0200

    fest instagram with socket

--- public/applications/instagram/common.js
+++ public/applications/instagram/common.js
@@ -2,7 +2,7 @@ var ids = new Array();
 var enlarged = 0;
 
 $(function(){
-
+	
 	$('#container').isotope({
 		itemSelector : '.item',
 		layoutMode : 'fitRows'
@@ -119,3 +119,19 @@ function findPhotosWithTag(tag, limit){
 	findPhotos(address, limit);
 }
 
+function getUrlVars() {
+	var vars = [], hash;
+	var i = 0;
+	var hashes = window.parent.location.href.slice(window.parent.location.href.indexOf('?') + 1).split('&');
+	for(i = 0; i < hashes.length; i++)
+	{
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
+}
+
+function lowerWithoutSpaces(input) {
+	return input.toLowerCase().split(' ').join('');
+}
