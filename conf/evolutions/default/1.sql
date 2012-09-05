8fa4507821fd229d9d7ec4deac5f5f777f341b3d
Marcello Romanelli <romanelm@Marcellos-MacBook-Pro.local>
2012-09-05 13:42:58 +0200

    stuff

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table display (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  latitude                  float,
+  longitude                 float,
+  constraint pk_display primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table display;
+
+SET FOREIGN_KEY_CHECKS=1;
+
