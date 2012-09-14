864732c7d5e4f97ae2f8976971353ff1e6bd87b7
ivanelhart <ivan.elhart@gmail.com>
2012-09-14 11:20:15 +0200

    Twitter2 - initial commit

---
+++ app/Global.java
@@ -0,0 +1,23 @@
+
+
+import controllers.AppTwitterController;
+import play.*;
+
+public class Global extends GlobalSettings {
+
+  @Override
+  public void onStart(Application app) {
+    Logger.info("Glogal: OnStart: Application has started...");
+    AppTwitterController.initTweeterSearch();
+    AppTwitterController.startTwitterStream();
+    //AppTwitterController.startTwitterScheduler();
+  }  
+  
+  @Override
+  public void onStop(Application app) {
+    Logger.info("Global: OnStop: Application shutdown...");
+    AppTwitterController.stopTwitterStream();
+    //AppTwitterController.stopTwitterScheduler();
+  }  
+    
+}
